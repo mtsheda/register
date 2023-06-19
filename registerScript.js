eb0299c0b9c3a2593058820ef12ae5609e39b8a9
mtsheda <tmafora2@gmail.com>
2023-06-19 10:31:08 +0200

    second commit

--- registerScript.js
+++ registerScript.js
@@ -1,73 +1,61 @@
-  
-  
-  document.getElementById('myForm').addEventListener('submit', function(event) {
-  event.preventDefault(); 
+document.getElementById('myForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  var name = document.getElementById('nameInput').value;
+  var surname = document.getElementById('surnameInput').value;
+  var email = document.getElementById("emailInput").value;
+  var password = document.getElementById("passwordInput").value;
+  var contact = document.getElementById("contactInput").value;
+  var confirmPassword = document.getElementById("confirmPasswordInput").value;
+
+  var storedData = localStorage.getItem("userData");
+  var users = storedData ? JSON.parse(storedData) : [];
+
+  const userData = {
+    name: name,
+    email: email,
+    password: password,
+    contact: contact,
+    confirmPassword: confirmPassword
+  };
 
   
-
-    var name = document.getElementById('nameInput').value;
-    var surname = document.getElementById('surnameInput').value;
-    var email = document.getElementById("emailInput").value;
-    var password = document.getElementById("passwordInput").value;
-    var contact = document.getElementById("contactInput").value;
-    var confirmPassword = document.getElementById("confirmPasswordInput").value;
-
-    
-
-    const userData = {
-      name: name,
-      email: email,
-      password: password,
-      conatact:contact,
-      confirmPassword:confirmPassword
-    };
-    
-    localStorage.setItem("userData", JSON.stringify(userData));
+  users.push(userData);
 
 
-  function addDataToTable(name,surname,email,password,contact,confirmPassword) {
+  localStorage.setItem("userData", JSON.stringify(users));
+
+  function addDataToTable(userData) {
     var table = document.getElementById('dataTable');
     var newRow = table.insertRow();
-  
+
     let nameCell = newRow.insertCell();
-    nameCell.textContent = name;
-  
-   var surnameCell = newRow.insertCell();
-  surnameCell.textContent = surname;
+    nameCell.textContent = userData.name;
 
-  var emailCell = newRow.insertCell();
-  emailCell.textContent = email;
+    let surnameCell = newRow.insertCell();
+    surnameCell.textContent = userData.surname;
 
-  var passwordCell = newRow.insertCell();
-  passwordCell.textContent = password;
+    let emailCell = newRow.insertCell();
+    emailCell.textContent = userData.email;
 
-  var contactCell = newRow.insertCell();
-  contactCell.textContent = contact;
+    let passwordCell = newRow.insertCell();
+    passwordCell.textContent = userData.password;
 
-  var confirmPasswordCell = newRow.insertCell();
-  confirmPasswordCell.textContent = confirmPassword;
+    let contactCell = newRow.insertCell();
+    contactCell.textContent = userData.contact;
+
+    let confirmPasswordCell = newRow.insertCell();
+    confirmPasswordCell.textContent = userData.confirmPassword;
   }
-  
 
-  
-    
-  
+  addDataToTable(userData);
+  document.getElementById('myForm').reset();
+});
 
-    addDataToTable(name,surname,email,password,contact,confirmPassword);
-    document.getElementById('myForm').reset(); 
-  });
-
-  function removeLastEntry(){
-
-    let table = document.getElementById('dataTable');
-    let counter = table.rows.length;
-    if (counter > 1){
-        table.deleteRow(counter-1)
-    }
-};
-
-  
-
-
-
-
+function removeLastEntry() {
+  let table = document.getElementById('dataTable');
+  let counter = table.rows.length;
+  if (counter > 1) {
+    table.deleteRow(counter - 1);
+  }
+}
